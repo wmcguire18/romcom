8c74275c3dc116bda60531aba272bbc4e5976c5f
Andrew Vallejo <andrew.a.vallejo@gmail.com>
2021-04-01 16:09:22 -0600

    Updatde HTML for consistency

--- src/main.js
+++ src/main.js
@@ -15,7 +15,7 @@ var makeButton = document.querySelector(".make-new-button")
 var savedCovers = [
   new Cover("http://3.bp.blogspot.com/-iE4p9grvfpQ/VSfZT0vH2UI/AAAAAAAANq8/wwQZssi-V5g/s1600/Do%2BNot%2BForsake%2BMe%2B-%2BImage.jpg","Sunsets and Sorrows", "sunsets", "sorrows")
 ]
-var currentCover
+var currentCover;
 
 // Event Listeners
 window.addEventListener("load", randomCover)
